e814c83766b45c4e9010ad787b5b6b94ef9e98d2
luyug <luyug@andrew.cmu.edu>
2021-06-16 10:23:47 -0400

    add new loss

--- src/grad_cache/loss.py
+++ src/grad_cache/loss.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import torch
 from torch import Tensor
 from torch.nn import functional as F
@@ -6,11 +8,12 @@ from torch import distributed as dist
 
 class SimpleContrastiveLoss:
     def __init__(self, n_hard_negatives: int = 0):
-        self.n_hn = n_hard_negatives
+        self.target_per_qry = n_hard_negatives + 1
 
     def __call__(self, x: Tensor, y: Tensor, target: Tensor = None, reduction: str = 'mean'):
         if target is None:
-            target = torch.arange(0, x.size(0)*(self.n_hn + 1), self.n_hn + 1, device=x.device)
+            assert x.size(0) * self.target_per_qry == y.size(0)
+            target = torch.arange(0, x.size(0) * self.target_per_qry, self.target_per_qry, device=x.device)
 
         logits = torch.matmul(x, y.transpose(0, 1))
         return F.cross_entropy(logits, target, reduction=reduction)
@@ -35,3 +38,26 @@ class DistributedContrastiveLoss(SimpleContrastiveLoss):
         dist.all_gather(gathered, t)
         gathered[self.rank] = t
         return torch.cat(gathered, dim=0)
+
+
+class ContrastiveLossWithQueryClosure(SimpleContrastiveLoss):
+    def __call__(
+            self,
+            *reps: Tensor,
+            query_closure: Callable[[], Tensor] = None,
+            target: Tensor = None,
+            reduction: str = 'mean'
+    ):
+        if len(reps) == 0 or len(reps) > 2:
+            raise ValueError(f'Expecting 1 or 2 tensor input, got {len(reps)} tensors')
+
+        # no closure evaluation
+        if len(reps) == 2:
+            assert query_closure is None, 'received 2 representation tensors while query_closure is also set'
+            return super().__call__(*reps, target=target, reduction=reduction)
+
+        # run the closure
+        assert query_closure is not None
+        x = query_closure()
+        y = reps[0]
+        return super().__call__(x, y, target=target, reduction=reduction)
