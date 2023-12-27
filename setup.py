136c460a176a088ae11db708a67219dfc88e2149
luyug <luyug@andrew.cmu.edu>
2023-12-27 16:49:47 -0500

    [jax] single decorator grad cache

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='GradCache',
-    version='0.0.1',
-    packages=['grad_cache'],
-    package_dir={'': 'src'},
+    version='0.1.0',
+    packages=['grad_cache', 'grad_cache.cachex'],
+    package_dir={'': 'src', 'grad_cache': 'src/grad_cache'},
     url='https://github.com/luyug/GradCache',
     license='Apache-2.0',
     author='Luyu Gao',
